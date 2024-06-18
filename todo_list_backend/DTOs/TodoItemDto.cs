4c1390610c05b2263cb024508d6dffd710d6b22d
mbaie <mbaie@K1>
2024-06-18 16:19:13 -0400

    Add project files.

---
+++ todo_list_backend/DTOs/TodoItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace todo_list_backend.DTOs;
+
+public class TodoItemDto
+{
+    public string Description { get; set; }
+    public string DueDate { get; set; }
+    public bool AreThereAdditionalDetails { get; set; }
+    public string AdditionalDetails { get; set; }
+}
